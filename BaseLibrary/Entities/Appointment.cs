4314ce2ba7693d94d068583e0dc38445869390c8
AmintasNeto <netorubok14@gmail.com>
2025-07-18 12:00:49 -0300

    Atualiza as entidades

--- BaseLibrary/Entities/Appointment.cs
+++ BaseLibrary/Entities/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,21 @@ namespace BaseLibrary.Entities
     public class Appointment
     {
         public int Id { get; set; }
-        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        public DateOnly AppointmentDate { get; set; }
+
+        [Required]
         public TimeSpan AppointmentStartTime { get; set; }
+
+        [Required]
         public TimeSpan AppointmentEndTime { get; set; }
+
+        [Required]
         public bool IsCanceled { get; set; } = false;
 
         public ApplicationUser? Doctor { get; set; }
+        [Required]
         public int DoctorId { get; set; }
 
         public ApplicationUser? Patient { get; set; }
